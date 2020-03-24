87f9fa6f4a596f0763d06dae0de1f3119effde19
Wenjie Zhang <19354817+myxxxsquared@users.noreply.github.com>
2020-03-24 19:46:44 +0800

    modify to code

--- mark.py
+++ mark.py
@@ -7,15 +7,13 @@ import csv
 import traceback
 import sys
 
-HTML_1 = r"""
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN" "http://www.w3.org/TR/REC-html40/strict.dtd">
-<html><head><meta name="qrichtext" content="1" /><style type="text/css">
-p, li { white-space: pre-wrap; }
-</style></head><body style=" font-family:'Ubuntu'; font-size:11pt; font-weight:400; font-style:normal;">
-<p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><span style=" font-family:'Ubuntu';">
+HTML_1 = r"""<!doctype html>
+<html><body>
+<pre>
 """
 
-HTML_2 = r"""</span></p></body></html>"""
+HTML_2 = r"""
+</pre></body></html>"""
 
 from PyQt5.QtWidgets import QWidget, QApplication, QFileDialog, QMessageBox
 
@@ -29,8 +27,12 @@ class MarkApp:
         self.ui.btn_save.clicked.connect(self.saveinfo)
         self.ui.btn_back.clicked.connect(self.btnprev)
         self.ui.btn_next.clicked.connect(self.btnnext)
-        self.ui.btn_yes.clicked.connect(self.btnyes)
-        self.ui.btn_no.clicked.connect(self.btnno)
+        self.ui.btn_1_yes.clicked.connect(self.btnyes_1)
+        self.ui.btn_1_no.clicked.connect(self.btnno_1)
+        self.ui.btn_2_yes.clicked.connect(self.btnyes_2)
+        self.ui.btn_2_no.clicked.connect(self.btnno_2)
+        self.ui.btn_3_yes.clicked.connect(self.btnyes_3)
+        self.ui.btn_3_no.clicked.connect(self.btnno_3)
         self.ui.btn_gencsv.clicked.connect(self.gencsv)
 
         self.filename = None
@@ -51,28 +53,11 @@ class MarkApp:
         csvfile = open(csvfile, "w", newline="")
         writer = csv.writer(csvfile)
 
-        writer.writerow(("", "uid", "word", "title", "cat"))
+        writer.writerow(("id", "gencomment", "gencode", "problem"))
 
         for i in range(len(self.items)):
-            yesno, word, title, cat = self.results[i]
-            uid, *_ = self.items[i]
-
-            if yesno is not True:
-                continue
-
-            if cat is None:
-                catt = ""
-            elif cat == 1:
-                catt = "社会防疫动态"
-            elif cat == 2:
-                catt = "大众防护指南"
-            elif cat == 3:
-                catt = "专业知识科普"
-            elif cat == 4:
-                catt = "抗击疫情故事"
-
-            writer.writerow(("", uid, word, title, catt))
-
+            yesno1, yesno2, yesno3 = self.results[i]
+            writer.writerow((i, yesno1, yesno2, yesno3))
         csvfile.close()
 
     def openfile(self):
@@ -101,7 +86,7 @@ class MarkApp:
 
         self.filename = filename
         self.items = items
-        self.results = results or [[None, "", "", None] for _ in range(len(items))]
+        self.results = results or [[None, None, None] for _ in range(len(items))]
         self.index = 0
 
         self.updateinfo()
@@ -137,24 +122,64 @@ class MarkApp:
         self.index += 1
         self.updateinfo()
 
-    def btnyes(self):
+    def btnyes_1(self):
         if self.items is None:
             return
-        
+
         yesno = True
         self.results[self.index][0] = yesno
-        self.ui.btn_yes.setStyleSheet("background: green;" if yesno is True else "")
-        self.ui.btn_no.setStyleSheet("background: red;" if yesno is False else "")
+        self.ui.btn_1_yes.setStyleSheet("background: green;" if yesno is True else "")
+        self.ui.btn_1_no.setStyleSheet("background: red;" if yesno is False else "")
         self.saveinfo()
 
-    def btnno(self):
+    def btnyes_2(self):
         if self.items is None:
             return
-        
+
+        yesno = True
+        self.results[self.index][1] = yesno
+        self.ui.btn_2_yes.setStyleSheet("background: green;" if yesno is True else "")
+        self.ui.btn_2_no.setStyleSheet("background: red;" if yesno is False else "")
+        self.saveinfo()
+    
+    def btnyes_3(self):
+        if self.items is None:
+            return
+
+        yesno = True
+        self.results[self.index][2] = yesno
+        self.ui.btn_3_yes.setStyleSheet("background: green;" if yesno is True else "")
+        self.ui.btn_3_no.setStyleSheet("background: red;" if yesno is False else "")
+        self.saveinfo()
+
+    def btnno_1(self):
+        if self.items is None:
+            return
+
         yesno = False
         self.results[self.index][0] = yesno
-        self.ui.btn_yes.setStyleSheet("background: green;" if yesno is True else "")
-        self.ui.btn_no.setStyleSheet("background: red;" if yesno is False else "")
+        self.ui.btn_1_yes.setStyleSheet("background: green;" if yesno is True else "")
+        self.ui.btn_1_no.setStyleSheet("background: red;" if yesno is False else "")
+        self.saveinfo()
+
+    def btnno_2(self):
+        if self.items is None:
+            return
+
+        yesno = False
+        self.results[self.index][1] = yesno
+        self.ui.btn_2_yes.setStyleSheet("background: green;" if yesno is True else "")
+        self.ui.btn_2_no.setStyleSheet("background: red;" if yesno is False else "")
+        self.saveinfo()
+    
+    def btnno_3(self):
+        if self.items is None:
+            return
+
+        yesno = False
+        self.results[self.index][2] = yesno
+        self.ui.btn_3_yes.setStyleSheet("background: green;" if yesno is True else "")
+        self.ui.btn_3_no.setStyleSheet("background: red;" if yesno is False else "")
         self.saveinfo()
 
 
@@ -162,71 +187,31 @@ class MarkApp:
         if self.items is None:
             return
 
-        title = self.ui.text_title.text()
-        word = self.ui.text_word.text()
-        cat = None
-        if self.ui.radio_1.isChecked():
-            cat = 1
-        if self.ui.radio_2.isChecked():
-            cat = 2
-        if self.ui.radio_3.isChecked():
-            cat = 3
-        if self.ui.radio_4.isChecked():
-            cat = 4
-        self.results[self.index][1] = word
-        self.results[self.index][2] = title
-        self.results[self.index][3] = cat
-
         self.savefile()
 
     def updateinfo(self):
         if self.items is not None:
-            yesno, word, title, cat = self.results[self.index]
-            uid, freq, words, title1, detail1, title2, detail2, title3, detail3 = self.items[self.index]
-            self.ui.btn_yes.setStyleSheet("background: green;" if yesno is True else "")
-            self.ui.btn_no.setStyleSheet("background: red;" if yesno is False else "")
-            self.ui.text_title.setText(title)
-            self.ui.text_word.setText(word)
-            self.ui.radio_1.setAutoExclusive(False)
-            self.ui.radio_2.setAutoExclusive(False)
-            self.ui.radio_3.setAutoExclusive(False)
-            self.ui.radio_4.setAutoExclusive(False)
-            self.ui.radio_1.setChecked(cat == 1)
-            self.ui.radio_2.setChecked(cat == 2)
-            self.ui.radio_3.setChecked(cat == 3)
-            self.ui.radio_4.setChecked(cat == 4)
-            self.ui.radio_1.setAutoExclusive(True)
-            self.ui.radio_2.setAutoExclusive(True)
-            self.ui.radio_3.setAutoExclusive(True)
-            self.ui.radio_4.setAutoExclusive(True)
-            self.ui.text_uid.setText(uid)
-            self.ui.text_freq.setText(str(freq))
-            self.ui.text_words.setText(words)
-            self.ui.text_title_1.setText(title1)
-            self.ui.text_title_2.setText(title2)
-            self.ui.text_title_3.setText(title3)
-            self.ui.text_detail_1.setHtml(HTML_1 + html.escape(detail1) + HTML_2)
-            self.ui.text_detail_2.setHtml(HTML_1 + html.escape(detail2) + HTML_2)
-            self.ui.text_detail_3.setHtml(HTML_1 + html.escape(detail3) + HTML_2)
+            yesno1, yesno2, yesno3 = self.results[self.index]
+            comment, code = self.items[self.index]
+            self.ui.btn_1_yes.setStyleSheet("background: green;" if yesno1 is True else "")
+            self.ui.btn_1_no.setStyleSheet("background: red;" if yesno1 is False else "")
+            self.ui.btn_2_yes.setStyleSheet("background: green;" if yesno2 is True else "")
+            self.ui.btn_2_no.setStyleSheet("background: red;" if yesno2 is False else "")
+            self.ui.btn_3_yes.setStyleSheet("background: green;" if yesno3 is True else "")
+            self.ui.btn_3_no.setStyleSheet("background: red;" if yesno3 is False else "")
+
+            self.ui.text_detail_1.setHtml(HTML_1 + html.escape(comment) + HTML_2)
+            self.ui.text_detail_2.setHtml(HTML_1 + html.escape(code) + HTML_2)
             self.ui.lab_info.setText(f"{self.index+1} / {len(self.items)}")
         else:
-            self.ui.btn_yes.setStyleSheet("")
-            self.ui.btn_no.setStyleSheet("")
-            self.ui.text_title.setText("")
-            self.ui.text_word.setText("")
-            self.ui.radio_1.setChecked(False)
-            self.ui.radio_2.setChecked(False)
-            self.ui.radio_3.setChecked(False)
-            self.ui.radio_4.setChecked(False)
-            self.ui.text_uid.setText("")
-            self.ui.text_freq.setText("")
-            self.ui.text_words.setText("")
-            self.ui.text_title_1.setText("")
-            self.ui.text_title_2.setText("")
-            self.ui.text_title_3.setText("")
+            self.ui.btn_1_yes.setStyleSheet("")
+            self.ui.btn_1_no.setStyleSheet("")
+            self.ui.btn_2_yes.setStyleSheet("")
+            self.ui.btn_2_no.setStyleSheet("")
+            self.ui.btn_3_yes.setStyleSheet("")
+            self.ui.btn_3_no.setStyleSheet("")
             self.ui.text_detail_1.setHtml(HTML_1 + HTML_2)
             self.ui.text_detail_2.setHtml(HTML_1 + HTML_2)
-            self.ui.text_detail_3.setHtml(HTML_1 + HTML_2)
             self.ui.lab_info.setText("0 / 0")
 
 def main():
--- mark_read.py
+++ mark_read.py
@@ -1,80 +1,11 @@
-
-import re
-
-re_line_topic = re.compile(r'Topic #([\d]+), freq=([\d\.]+), words: (.+)')
-
-def splititems(it):
-    lines = []
-
-    # 0: waiting
-    # 1: after "================"
-    # 2: reading
-    status = 0
-
-    while True:
-        try:
-            line = next(it)
-        except StopIteration:
-            if lines:
-                yield lines
-            break
-
-        line = line.strip()
-
-        if line == "===========================================":
-            if status == 2:
-                status = 0
-                if lines:
-                    yield lines
-                    lines = []
-            status = 1
-        else:
-            if status == 1:
-                if line.startswith("uid: "):
-                    status = 2
-                else:
-                    status = 0
-            if status == 2:
-                lines.append(line)
-    
-
-def readitem(it):
-    re_line_topic_local = re_line_topic
-
-    for item in splititems(it):
-        assert item[0].startswith('uid: ') and len(item[0]) == 41
-        uid = item[0][5:]
-
-        itemlen = len(item)
-
-        assert itemlen == 6 or itemlen == 9 or itemlen == 12
-
-        match = re_line_topic_local.match(item[1])
-        assert match
-        freq = float(match.group(2))
-        words = match.group(3)
-
-        assert item[2].startswith("last phrase:")
-
-        assert item[3].startswith("Similarlity = ")
-        title1 = item[4]
-        detail1 = item[5]
-
-        if itemlen > 6:
-            assert item[6].startswith("Similarlity = ")
-        title2 = item[7] if itemlen > 7 else ""
-        detail2 = item[8] if itemlen > 8 else ""
-
-        if itemlen > 9:
-            assert item[9].startswith("Similarlity = ")
-        title3 = item[10] if itemlen > 10 else ""
-        detail3 = item[11] if itemlen > 11 else ""
-
-        yield uid, freq, words, title1, detail1, title2, detail2, title3, detail3
+import json
 
 def readfile(filename):
     f = open(filename, encoding='utf-8')
-    f = iter(f)
-    return readitem(f)
-
-
+    f = json.load(f)
+    com = f['comment']
+    fun = f['function']
+    keys = com.keys()
+    sortedkeys = sorted(map(int, com.keys()))
+    assert sortedkeys == list(range(len(keys)))
+    return [(com[k], fun[k]) for k in keys]
